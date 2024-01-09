1569821b676b39a2a9cf89e9553d93979e880fe3
xiaodch <1754859473@qq.com>
2024-01-09 12:21:37 -0500

    check

--- 1.c
+++ 1.c
@@ -3,7 +3,7 @@ int main(int argc, const char *argv[])
 {
 	printf("hello\n");
 	printf("hello linux\n");
-	printf("hello xiao\n");
+	printf("hello xiao 11111\n");
 	printf("111111\n");
 	return 0;
 }
